8a5756c85eefb257e19fa0e91b6321a3412d2bb0
Konstantin Sopin <sopinorg@gmail.com>
2022-08-18 20:45:50 +0100

    unbind interm deps from the NOTGate struct

--- gate/not_gate.go
+++ gate/not_gate.go
@@ -5,17 +5,17 @@ import "github.com/k8ns/computer_from_scratch/wire"
 type NOTGate struct {
 	I        *wire.Pin
 	O        *wire.Pin
-	nandGate *NANDGate
 }
 
 func NewNOTGate() *NOTGate {
 	gate := &NOTGate{
 		I:        &wire.Pin{},
 		O:        &wire.Pin{},
-		nandGate: NewNANDGate(),
 	}
 
-	wire.Connect(gate.I, gate.nandGate.A, gate.nandGate.B)
-	wire.Connect(gate.nandGate.O, gate.O)
+    nandGate := NewNANDGate()
+
+	wire.Connect(gate.I, nandGate.A, nandGate.B)
+	wire.Connect(nandGate.O, gate.O)
 	return gate
 }
